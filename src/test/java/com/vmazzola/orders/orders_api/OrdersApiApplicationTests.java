77a3c98370df2de1e3028c05e6e6a525c5eab45f
Victor <victormazzola2019@gmail.com>
2025-12-07 15:23:58 -0300

    feat: initial Spring Boot project setup

---
+++ src/test/java/com/vmazzola/orders/orders_api/OrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmazzola.orders.orders_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
